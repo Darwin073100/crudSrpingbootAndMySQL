4979bcb0315e52c5e428668ae20ffac35e4ad130
Darwin073100 <edwinegq@outlook.com>
2022-03-05 20:50:55 -0600

    Actualization of register

--- src/main/java/com/sinfloo/demo/controler/Controlador.java
+++ src/main/java/com/sinfloo/demo/controler/Controlador.java
@@ -8,10 +8,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping
@@ -36,5 +38,12 @@ public class Controlador {
     public String save(@Validated Persona p, Model model){
         service.save(p);
         return "redirect:/listar";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editar(@PathVariable int id, Model model){
+        Optional<Persona> persona = service.listarId(id);
+        model.addAttribute("persona", persona);
+        return "form";
     }
 }
--- src/main/java/com/sinfloo/demo/service/PersonaService.java
+++ src/main/java/com/sinfloo/demo/service/PersonaService.java
@@ -22,7 +22,7 @@ public class PersonaService implements IpersonaService {
 
     @Override
     public Optional<Persona> listarId(int id) {
-        return Optional.empty();
+        return data.findById(id);
     }
 
     @Override
